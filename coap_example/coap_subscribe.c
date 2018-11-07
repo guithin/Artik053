4d892d5ad109521ce5d9e9b0f4133bc7b9d8a95d
Jinwon Jung <on14@naver.com>
2018-11-07 12:32:51 +0900

    Create coap_subscribe.c

---
+++ coap_example/coap_subscribe.c
@@ -0,0 +1,283 @@
+#include "wifi.h"
+#include <tinyara/gpio.h>
+#include <apps/netutils/dhcpc.h>
+
+#include <tinyara/analog/adc.h>
+#include <tinyara/analog/ioctl.h>
+
+#include <apps/shell/tash.h>
+
+// for NTP
+#include <apps/netutils/ntpclient.h>
+#include <apps/netutils/er-coap/er-coap-13.h>
+
+#include <apps/netutils/cJSON.h>
+
+#include <sys/socket.h>
+
+#define DEFAULT_CLIENT_ID "123456789"
+#define SERVER_ADDR ""
+
+#define SERVER_PORT 
+
+#define NET_DEVNAME "wl1"
+
+static const char mqtt_ca_cert_str[] = \
+		"-----BEGIN CERTIFICATE-----\r\n"
+		"MIIGrTCCBZWgAwIBAgIQASAP9e8Tbenonqd/EQFJaDANBgkqhkiG9w0BAQsFADBN\r\n"
+		"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMScwJQYDVQQDEx5E\r\n"
+		"aWdpQ2VydCBTSEEyIFNlY3VyZSBTZXJ2ZXIgQ0EwHhcNMTgwMzA4MDAwMDAwWhcN\r\n"
+		"MjAwNDA1MTIwMDAwWjBzMQswCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5p\r\n"
+		"YTERMA8GA1UEBxMIU2FuIEpvc2UxJDAiBgNVBAoTG1NhbXN1bmcgU2VtaWNvbmR1\r\n"
+		"Y3RvciwgSW5jLjEWMBQGA1UEAwwNKi5hcnRpay5jbG91ZDCCASIwDQYJKoZIhvcN\r\n"
+		"AQEBBQADggEPADCCAQoCggEBANghNaTXWDfYV/JWgBnX4hmhcClPSO0onx5B2url\r\n"
+		"YzpvTc3MBaQ+08YBpAKvTqZvPqrJUIM45Q91M301I5e2kz0DMq2zQZOGB0B83V/O\r\n"
+		"O4vwETq4PCjAPhMinF4dN6HeJCuqo1CLh8evhfkFiJvpEfQWTxdjzPJ0Zdj/2U8E\r\n"
+		"8Ht7zV5pWiDtuejtIDHB5H6fCx4xeQy/E+5l4V6R3BnRKpZsJtlhTh0RFqWhw5DJ\r\n"
+		"/WWpGP//1VTZSHyW9SABsPd+jP1YgDraRD4b4lZBU6c8nC5qT3dhdiYoG6xUgTb3\r\n"
+		"kfgUhhlOFpe3sBtR32OS8RuFrFeQDGaa3r6pfSy06Kph/eECAwEAAaOCA2EwggNd\r\n"
+		"MB8GA1UdIwQYMBaAFA+AYRyCMWHVLyjnjUY4tCzhxtniMB0GA1UdDgQWBBSNBf6r\r\n"
+		"7S/j0oV3A0XmEflXErutQDAlBgNVHREEHjAcgg0qLmFydGlrLmNsb3VkggthcnRp\r\n"
+		"ay5jbG91ZDAOBgNVHQ8BAf8EBAMCBaAwHQYDVR0lBBYwFAYIKwYBBQUHAwEGCCsG\r\n"
+		"AQUFBwMCMGsGA1UdHwRkMGIwL6AtoCuGKWh0dHA6Ly9jcmwzLmRpZ2ljZXJ0LmNv\r\n"
+		"bS9zc2NhLXNoYTItZzYuY3JsMC+gLaArhilodHRwOi8vY3JsNC5kaWdpY2VydC5j\r\n"
+		"b20vc3NjYS1zaGEyLWc2LmNybDBMBgNVHSAERTBDMDcGCWCGSAGG/WwBATAqMCgG\r\n"
+		"CCsGAQUFBwIBFhxodHRwczovL3d3dy5kaWdpY2VydC5jb20vQ1BTMAgGBmeBDAEC\r\n"
+		"AjB8BggrBgEFBQcBAQRwMG4wJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2lj\r\n"
+		"ZXJ0LmNvbTBGBggrBgEFBQcwAoY6aHR0cDovL2NhY2VydHMuZGlnaWNlcnQuY29t\r\n"
+		"L0RpZ2lDZXJ0U0hBMlNlY3VyZVNlcnZlckNBLmNydDAJBgNVHRMEAjAAMIIBfwYK\r\n"
+		"KwYBBAHWeQIEAgSCAW8EggFrAWkAdgCkuQmQtBhYFIe7E6LMZ3AKPDWYBPkb37jj\r\n"
+		"d80OyA3cEAAAAWIHFb1dAAAEAwBHMEUCIQCQ0UjVVJSQDRB3oxzI5aD1Hs5GhbXj\r\n"
+		"I6Cqt3/tkXT1WQIgNVWRgbJ72Ik9gp5QoNxhCZ+h//or0uL7PHnv3cP5L9UAdgBv\r\n"
+		"U3asMfAxGdiZAKRRFf93FRwR2QLBACkGjbIImjfZEwAAAWIHFb73AAAEAwBHMEUC\r\n"
+		"IQDxCxJCsZjuqbQvuwipgdUf1l6qXdiekM5zn33i1+KYxgIgKDMJEuKHzhkweT2S\r\n"
+		"Y4dWBuzSdOAzZfoDrIGdsFvkxi0AdwC72d+8H4pxtZOUI5eqkntHOFeVCqtS6BqQ\r\n"
+		"lmQ2jh7RhQAAAWIHFb1YAAAEAwBIMEYCIQCNDYdxWmqUGGwNzXlJ1/NXxzwqPYIB\r\n"
+		"eSJDuR1xfWtSsQIhAJsygf2rqPS+O7qQAzggCQ2V/3JDRUhuxNDPqwooo47uMA0G\r\n"
+		"CSqGSIb3DQEBCwUAA4IBAQBvRGWibvHFrRUWsArJ9lmS5MMZFbXXQPXbflgv3nSG\r\n"
+		"ShmhBC3o+k97J0Wgp/wH7uDf01RrRMAVNm458g1Mr4AMAXq3zzxNNTwjGYw/USuG\r\n"
+		"UprrKqc9onugtAUX8DGvlZr8SWO3FhPlyamWQ69jutx/X4nfHyZr41bX9WQ/ay0F\r\n"
+		"GQJ1tRTrX1eUPO+ucXeG8vTbt09bRNnoY+i97dzrwHakXySfHohNsIbwmrsS4SQv\r\n"
+		"7eG9g5+5vsc2B9ugGcELIYKrzDWNPshir37KSpcwLUCmDJkTQp8+KhJUKgbTALTa\r\n"
+		"nxuDyNwZIwW66vv1t0Zi4vKU8hfUsAN2N3wcsb6pY/RA\r\n"
+		"-----END CERTIFICATE-----\r\n"
+		"-----BEGIN CERTIFICATE-----\r\n"
+		"MIIElDCCA3ygAwIBAgIQAf2j627KdciIQ4tyS8+8kTANBgkqhkiG9w0BAQsFADBh\r\n"
+		"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\r\n"
+		"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\r\n"
+		"QTAeFw0xMzAzMDgxMjAwMDBaFw0yMzAzMDgxMjAwMDBaME0xCzAJBgNVBAYTAlVT\r\n"
+		"MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxJzAlBgNVBAMTHkRpZ2lDZXJ0IFNIQTIg\r\n"
+		"U2VjdXJlIFNlcnZlciBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\r\n"
+		"ANyuWJBNwcQwFZA1W248ghX1LFy949v/cUP6ZCWA1O4Yok3wZtAKc24RmDYXZK83\r\n"
+		"nf36QYSvx6+M/hpzTc8zl5CilodTgyu5pnVILR1WN3vaMTIa16yrBvSqXUu3R0bd\r\n"
+		"KpPDkC55gIDvEwRqFDu1m5K+wgdlTvza/P96rtxcflUxDOg5B6TXvi/TC2rSsd9f\r\n"
+		"/ld0Uzs1gN2ujkSYs58O09rg1/RrKatEp0tYhG2SS4HD2nOLEpdIkARFdRrdNzGX\r\n"
+		"kujNVA075ME/OV4uuPNcfhCOhkEAjUVmR7ChZc6gqikJTvOX6+guqw9ypzAO+sf0\r\n"
+		"/RR3w6RbKFfCs/mC/bdFWJsCAwEAAaOCAVowggFWMBIGA1UdEwEB/wQIMAYBAf8C\r\n"
+		"AQAwDgYDVR0PAQH/BAQDAgGGMDQGCCsGAQUFBwEBBCgwJjAkBggrBgEFBQcwAYYY\r\n"
+		"aHR0cDovL29jc3AuZGlnaWNlcnQuY29tMHsGA1UdHwR0MHIwN6A1oDOGMWh0dHA6\r\n"
+		"Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEdsb2JhbFJvb3RDQS5jcmwwN6A1\r\n"
+		"oDOGMWh0dHA6Ly9jcmw0LmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEdsb2JhbFJvb3RD\r\n"
+		"QS5jcmwwPQYDVR0gBDYwNDAyBgRVHSAAMCowKAYIKwYBBQUHAgEWHGh0dHBzOi8v\r\n"
+		"d3d3LmRpZ2ljZXJ0LmNvbS9DUFMwHQYDVR0OBBYEFA+AYRyCMWHVLyjnjUY4tCzh\r\n"
+		"xtniMB8GA1UdIwQYMBaAFAPeUDVW0Uy7ZvCj4hsbw5eyPdFVMA0GCSqGSIb3DQEB\r\n"
+		"CwUAA4IBAQAjPt9L0jFCpbZ+QlwaRMxp0Wi0XUvgBCFsS+JtzLHgl4+mUwnNqipl\r\n"
+		"5TlPHoOlblyYoiQm5vuh7ZPHLgLGTUq/sELfeNqzqPlt/yGFUzZgTHbO7Djc1lGA\r\n"
+		"8MXW5dRNJ2Srm8c+cftIl7gzbckTB+6WohsYFfZcTEDts8Ls/3HB40f/1LkAtDdC\r\n"
+		"2iDJ6m6K7hQGrn2iWZiIqBtvLfTyyRRfJs8sjX7tN8Cp1Tm5gr8ZDOo0rwAhaPit\r\n"
+		"c+LJMto4JQtV05od8GiG7S5BNO98pVAdvzr508EIDObtHopYJeS4d60tbvVS3bR0\r\n"
+		"j6tJLp07kzQoH3jOlOrHvdPJbRzeXDLz\r\n"
+		"-----END CERTIFICATE-----\r\n";
+
+int currentLED = 0;
+
+struct ntpc_server_conn_s g_server_conn[2];
+
+const unsigned char *get_ca_cert(void) {
+	return (const unsigned char*)mqtt_ca_cert_str;
+}
+
+// Write the value of given gpio port.
+void gpio_write(int port, int value) {
+    char str[4];
+    static char devpath[16];
+    snprintf(devpath, 16, "/dev/gpio%d", port);
+    int fd = open(devpath, O_RDWR);
+
+    ioctl(fd, GPIOIOC_SET_DIRECTION, GPIO_DIRECTION_OUT);
+    write(fd, str, snprintf(str, 4, "%d", value != 0) + 1);
+
+    close(fd);
+}
+
+static void ntp_link_error(void)
+{
+	printf("ntp_link_error() callback is called.\n");
+}
+
+
+int main(int argc, FAR char *argv[]) {
+    bool wifiConnected = false;
+
+    char *strTopicAct = (char*)malloc(sizeof(char)*256);
+    sprintf(strTopicAct, "/v1.1/actions/7");
+
+    // for NTP Client
+    memset(&g_server_conn, 0, sizeof(g_server_conn));
+    g_server_conn[0].hostname = "0.asia.pool.ntp.org";
+    g_server_conn[0].port = 123;
+    g_server_conn[1].hostname = "1.asia.pool.ntp.org";
+    g_server_conn[1].port = 123;
+
+    int ret;
+
+    while(!wifiConnected) {
+        if (start_wifi_interface() == SLSI_STATUS_ERROR) {
+            printf("Connect Wi-Fi failed. Try Again.\n");
+        }
+        else {
+            wifiConnected = true;
+
+        }
+    }
+
+    printf("Connect to Wi-Fi success\n");
+
+    bool mqttConnected = false;
+    bool ipObtained = false;
+    printf("Get IP address\n");
+
+    struct dhcpc_state state;
+    void *dhcp_handle;
+
+    while(!ipObtained) {
+        dhcp_handle = dhcpc_open(NET_DEVNAME);
+        ret = dhcpc_request(dhcp_handle, &state);
+        dhcpc_close(dhcp_handle);
+
+        if (ret != OK) {
+            printf("Failed to get IP address\n");
+            printf("Try again\n");
+            sleep(1);
+        }
+        else {
+            ipObtained = true;
+        }
+    }
+    netlib_set_ipv4addr(NET_DEVNAME, &state.ipaddr);
+    netlib_set_ipv4netmask(NET_DEVNAME, &state.netmask);
+    netlib_set_dripv4addr(NET_DEVNAME, &state.default_router);
+
+    printf("IP address  %s\n", inet_ntoa(state.ipaddr));
+
+    up_mdelay(2000);
+
+    int ret_ntp = ntpc_start(g_server_conn, 2, 1000, ntp_link_error);
+    printf("ret: %d\n", ret_ntp);
+
+    // Connect to the WiFi network for Internet connectivity
+    printf("coap client tutorial\n");
+
+    // coap_packet
+    coap_packet_t coap_packet;
+
+
+    // Read analog signal using ADC
+    int fd;
+    struct adc_msg_s sample;
+    size_t readsize;
+    ssize_t nbytes;
+    int publishedVal = -1;
+
+    fd = open("/dev/adc0", O_RDONLY);
+    if (fd < 0) {
+        printf("%s: open failed: %d\n", __func__, errno);
+        return 0;
+    }
+
+    coap_packet_t* request;
+
+    request = (coap_packet_t*)malloc(sizeof(coap_packet_t));
+    struct sockaddr_in  server_addr;
+    int res = socket(PF_INET, SOCK_DGRAM, 0);
+    if (res == -1) {
+      		printf("socket failed");
+       		return 0;
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    unsigned int server_addr_len = sizeof(server_addr);
+    char recv[100]={0}; int flag=0;
+    while(flag == 0) {
+    	usleep(1*1000*1000);
+
+    	readsize = sizeof(struct adc_msg_s);
+
+    	// adc_msg_s structure returns the data structure containing analog readings from ADC pins
+    	coap_init_message(request, COAP_UDP, COAP_TYPE_CON, COAP_GET, coap_get_mid());
+    	coap_set_header_uri_path(request, strTopicAct);
+    	coap_set_header_uri_host(request, SERVER_ADDR);
+    	coap_set_header_observe(request, 0);
+    	request->options_len=50;
+
+    	char coaps[101]={0};
+    	int i;
+    	coap_serialize_message(request, coaps);
+    	for(i=100;i;i--){
+    		if(coaps[i]!=0){
+    			break;
+    		}
+    	}
+
+    	int send_len = sendto(res, (const void*)coaps, i + 1, 0,(const struct sockaddr*)&server_addr, server_addr_len);
+    	if (send_len == -1) {
+    		printf("send failed");
+    		continue;
+    	}
+    	printf("send success\n");
+    	int recv_len = recvfrom(res, recv, 100, 0, (struct sockaddr*)&server_addr, &server_addr_len);
+    	if(recv_len<0){
+    		printf("recv fail\n");
+    		continue;
+    	}
+    	else{
+    		recv[recv_len]=0;
+    		if(recv[1]==69) flag=1;
+    		else printf("err %d\n", recv[1]);
+    	}
+    }
+    printf("coap-server connect\n");
+	while(flag == 1){
+        printf("waitting...\n");
+		int recv_len = recvfrom(res, recv, 100, 0, (struct sockaddr*)&server_addr, &server_addr_len);
+		if(recv_len<0){
+			printf("recv fail\n");
+			continue;
+		}
+		recv[recv_len]=0;
+		printf("%d\n", recv_len);
+		if(recv[1]!=69){//code: 69 -> connected
+			printf("err %d\n", (int)recv[1]);
+			continue;
+		}
+		int i=0;
+		while(recv[i++]!=(int)0xff);//go to payload
+		printf("\n%s\n", recv+i);
+		char temp[50]={0};
+
+		//static format ack
+		sscanf(recv + i, "{\"actions\":[{\"name\":\"led\",\"parameters\":{\"OnOff\":%s}}]}", temp);
+		if(strncmp(temp, "false", 5)==0){
+			gpio_write(29, 0);
+		}
+		if(strncmp(temp, "true", 4)==0){
+			gpio_write(29, 1);
+		}
+	}
+	printf("err\n");
+    close(fd);
+}
